f56ec0111b098c6fb8c8c17b66ab2ca91f320ee1
Sergey Dokukin <swdokukin@gmail.com>
2018-12-11 12:53:28 +0200

    Initial Commit

---
+++ CurryGen/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CurryGen
+//
+//  Created by Sergey Dokukin on 12/11/18.
+//  Copyright © 2018 Sergey Dokukin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
